f144dcc1b24e4b66f484c8d35ad3b16d2a31d36f
Kevin Maduro <nivekzin@gmail.com>
2019-02-26 15:09:30 -0300

    library: Added a dumb component to test relative path

--- example/src/App.js
+++ example/src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react'
 
-import ExampleComponent from 'boilerplate'
+import { ExampleComponent, Button } from 'boilerplate'
 
 export default class App extends Component {
   render () {
     return (
       <div>
         <ExampleComponent text='Modern React component module' />
+        <Button />
       </div>
     )
   }
